739b6d671f00c1b82c4f81d9a1c43fa5a114e43c
legitbeep <glorytoharsh@gmail.com>
2021-11-23 15:57:45 +0530

    feat: added pre commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+/** @type {import('@commitlint/types').UserConfig} */
+const CommitLintConfiguration = {
+  extends: ["@commitlint/config-conventional"],
+  // add your own scope here if needed
+  // "scope-enum": [2, "always", ["components", "pages", "utils"]],
+  "scope-case": [2, "always", "kebab-case"],
+};
+
+module.exports = CommitLintConfiguration;
