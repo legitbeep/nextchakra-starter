2d00fb0a1622b239649648f9d13552dedfb94605
legitbeep <glorytoharsh@gmail.com>
2021-11-25 16:11:18 +0530

    feat: add custom theme and complete the layout for default app

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,16 @@ import Link from "next/link";
 
 import ThemeToggle from "./ThemeToggle";
 
-const Header = () => <Flex as="header" width="full" align="center"></Flex>;
+const Header = () => (
+  <Flex as="header" width="full" align="center">
+    <Heading as="h1" size="md">
+      <Link href="/">nextchakra-starter</Link>
+    </Heading>
+
+    <Box marginLeft="auto">
+      <ThemeToggle />
+    </Box>
+  </Flex>
+);
 
 export default Header;
