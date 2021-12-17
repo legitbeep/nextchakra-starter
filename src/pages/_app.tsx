4c368674df121a66bd73f7b67958d1d9b39e71b5
Harsh Yadav <50167891+legitbeep@users.noreply.github.com>
2021-12-17 12:09:02 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({
   return (
       <ChakraProvider theme={customTheme}>
         <Head>
-          <meta name="viewport" content="" />
+          <meta name="viewport" content="minimum-scale=1, maximum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
         </Head>
         <DefaultSeo {...defaultSeoConfig} />
         <Layout>
